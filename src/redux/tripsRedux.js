d551d2ac7ffbc8d579372caab6e3038c3e84f4c6
Oskar Rykowski <oskar.michal.rykowski@gmail.com>
2021-09-17 10:46:49 +0200

    Include Alex comments

--- src/redux/tripsRedux.js
+++ src/redux/tripsRedux.js
@@ -1,56 +1,55 @@
 /* SELECTORS */
 
-export const getAllTrips = ({trips}) => trips;
+export const getAllTrips = ({ trips }) => trips;
 
-export const getFilteredTrips = ({trips, filters}) => {
+export const getFilteredTrips = ({ trips, filters }) => {
   let output = trips;
 
   // filter by search phrase
-  if(filters.searchPhrase){
+  if (filters.searchPhrase) {
     const pattern = new RegExp(filters.searchPhrase, 'i');
-    output = output.filter(trip => pattern.test(trip.name));
+    output = output.filter((trip) => pattern.test(trip.name));
   }
 
   // TODO - filter by duration
-  if(filters.duration){
-    output = output.filter(trip => 
-      trip.days > filters.duration.from
-      &&
-      trip.days < filters.duration.to
-      );
-    }
+  if (filters.duration) {
+    output = output.filter(
+      (trip) =>
+        trip.days >= filters.duration.from && trip.days <= filters.duration.to
+    );
+  }
   // TODO - filter by tags
-if(filters.tags.length){
-  output = output.filter(trip => trip.tags.some(item => (filters.tags.includes(item))));
-}
+  if (filters.tags.length) {
+    output = output.filter((trip) =>
+      filters.tags.every((item) => trip.tags.includes(item))
+    );
+  }
 
-// function findCommonElements3(arr1, arr2) {
-//   return arr1.some(item => arr2.includes(item))
-// }
+  // function findCommonElements3(arr1, arr2) {
+  //   return arr1.some(item => arr2.includes(item))
+  // }
 
   // TODO - sort by cost descending (most expensive goes first)
 
   return output;
 };
 
-export const getTripById = ({trips}, tripId) => {
-  const filtered = trips.filter(
-  (trip) => trip.id === tripId ); 
+export const getTripById = ({ trips }, tripId) => {
+  const filtered = trips.filter((trip) => trip.id === tripId);
 
   // TODO - filter trips by tripId
 
   console.log('filtering trips by tripId:', tripId, filtered);
-  return filtered.length ? filtered[0] : {error: true};
+  return filtered.length ? filtered[0] : { error: true };
 };
 
-export const getTripsForCountry = ({trips}, countryCode) => {
-  const filtered = trips.filter(
-    (trip) => trip.country.code === countryCode );
+export const getTripsForCountry = ({ trips }, countryCode) => {
+  const filtered = trips.filter((trip) => trip.country.code === countryCode);
 
   // TODO - filter trips by countryCode
 
   console.log('filtering trips by countryCode:', countryCode, filtered);
-  return filtered.length ? filtered : [{error: true}];
+  return filtered.length ? filtered : [{ error: true }];
 };
 
 /* ACTIONS */
