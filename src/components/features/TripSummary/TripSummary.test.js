2812c1234b54cf641f741a50727fc9dcf5970f3f
Oskar Rykowski <oskar.michal.rykowski@gmail.com>
2021-09-20 17:23:14 +0200

    Working on tests

--- src/components/features/TripSummary/TripSummary.test.js
+++ src/components/features/TripSummary/TripSummary.test.js
@@ -1,13 +1,23 @@
-// import React from 'react';
-// import { shallow } from 'enzyme';
-// import TripSummary from './TripSummary';
+import React from 'react';
+import { shallow } from 'enzyme';
+import TripSummary from './TripSummary';
 
-// describe('Component TripSummary', () => {
-//   it('should generate right address', () => {
-//     const expectedId = 'abc';
-//     // const fakeTagsTable = ['tag1', 'tag2', 'tag3'];
-//     const expectedAddress = `/trip/${expectedId}`;
-//     const component = shallow(<TripSummary key={expectedId} />);
-//     expect(component.find('.link').prop('href')).toEqual(expectedAddress);
-//   });
-// });
+describe('Component TripSummary', () => {
+  it('should generate right address', () => {
+    const id = `abc`;
+    // const fakeTagsTable = ['tag1', 'tag2', 'tag3'];
+    const expectedAddress = `/trip/${id}`;
+    const component = shallow(<TripSummary key={id} />);
+    expect(component.find('Link').prop('to')).toEqual(expectedAddress);
+  });
+
+  it('should have correct src and alt', () => {
+    const imageFile = 'image.jpg';
+    const tripName = 'tripName';
+    const component = shallow(
+      <TripSummary image={imageFile} name={tripName} />
+    );
+    expect(component.find('img').prop('src')).toEqual(imageFile);
+    expect(component.find('img').prop('alt')).toEqual(tripName);
+  });
+});
