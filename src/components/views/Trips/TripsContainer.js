46239f8484983897c3c51e63578ed82a7f687e7c
Oskar Rykowski <oskar.michal.rykowski@gmail.com>
2021-09-21 16:14:07 +0200

    To be reverse

--- src/components/views/Trips/TripsContainer.js
+++ src/components/views/Trips/TripsContainer.js
@@ -1,14 +1,9 @@
 import { connect } from 'react-redux';
 import Trips from './Trips';
-import { getFilteredTrips, setTrip } from '../../../redux/tripsRedux';
+import { getFilteredTrips } from '../../../redux/tripsRedux';
 
 const mapStateToProps = (state) => ({
   trips: getFilteredTrips(state),
-  setTrip: setTrip,
 });
 
-const mapDispatchToProps = (dispatch) => ({
-  setTrip: (trip) => dispatch(setTrip(trip)),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(Trips);
+export default connect(mapStateToProps)(Trips);
