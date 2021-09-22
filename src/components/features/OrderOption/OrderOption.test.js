a3ec5bf6e9652db75d53293f9af259dc3c330ea0
Oskar Rykowski <oskar.michal.rykowski@gmail.com>
2021-09-22 19:36:28 +0200

    testing prosp OrderOption

--- src/components/features/OrderOption/OrderOption.test.js
+++ src/components/features/OrderOption/OrderOption.test.js
@@ -1,12 +1,20 @@
-// import React from 'react';
-// import { shallow } from 'enzyme';
-// import OrderOption from './OrderOption';
+import React from 'react';
+import { shallow } from 'enzyme';
+import { OrderOption } from './OrderOption';
 
-// describe('Component OrderOption', () => {
-//   const testType = 'number';
-//   const testName = 'Robert';
-//   it('should render without crashing', () => {
-//     const component = shallow(<OrderOption type={testType} name={testName} />);
-//     expect(component).toBeTruthy();
-//   });
-// });
+describe('Component OrderOption', () => {
+  const testType = 'number';
+  const testName = 'Robert';
+  it('should render without crashing', () => {
+    const component = shallow(<OrderOption type={testType} name={testName} />);
+    expect(component).toBeTruthy();
+  });
+  it('should return empty object if called without required props', () => {
+    const component = shallow(<OrderOption />);
+    expect(component).toEqual({});
+  });
+  it('should render props name correctly', () => {
+    const component = shallow(<OrderOption type={testType} name={testName} />);
+    expect(component.find('.title').text()).toEqual(testName);
+  });
+});
