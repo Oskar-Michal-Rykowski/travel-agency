8166e6d6f0d5861cc9223ab306c58039740abe24
Oskar Rykowski <oskar.michal.rykowski@gmail.com>
2021-09-25 13:41:49 +0200

    Working on tests

--- src/components/features/OrderOption/OrderOption.test.js
+++ src/components/features/OrderOption/OrderOption.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { OrderOption } from './OrderOption';
+// import DatePicker from 'react-datepicker';
 
 describe('Component OrderOption', () => {
   const testType = 'number';
@@ -95,6 +96,7 @@ for (let type in optionTypes) {
         /*test for dropdown */
         it('contains select and options', () => {
           const select = renderedSubcomponent.find('select');
+          //tutaj chodzi o to czy select zawiera dwa elementy?
           expect(select.length).toBe(1);
 
           const emptyOption = select.find('option[value=""]').length;
@@ -115,6 +117,126 @@ for (let type in optionTypes) {
             [mockProps.id]: testValue,
           });
         });
+        break;
+      }
+      /*test for number */
+      case 'number': {
+        it('contains input with correct props', () => {
+          const input = renderedSubcomponent.find('input');
+          expect(input.length).toBe(1);
+
+          expect(input.prop('value')).toBe(
+            mockPropsForType.number.currentValue
+          );
+          expect(input.prop('min')).toBe(mockProps.limits.min);
+          expect(input.prop('max')).toBe(mockProps.limits.max);
+          expect(input.hasClass('inputSmall')).toBe(true);
+        });
+
+        it('should run setOrderOption function on change', () => {
+          renderedSubcomponent
+            .find('input')
+            .simulate('change', { currentTarget: { value: testValueNumber } });
+          expect(mockSetOrderOption).toBeCalledTimes(1);
+          expect(mockSetOrderOption).toBeCalledWith({
+            [mockProps.id]: testValueNumber,
+          });
+        });
+
+        break;
+      }
+      /*test for number */
+      case 'text': {
+        it('contains input with correct props', () => {
+          const input = renderedSubcomponent.find('input');
+          expect(input.length).toBe(1);
+          //coś jest poniżej nie tak... czy musimy testować id? W inspektorze nie widać go jako propsa OrderOptiotText.
+          // expect(input.prop('id')).toBe(mockProps.id);
+          expect(input.hasClass('input')).toBe(true);
+        });
+
+        it('should run setOrderOption function on change', () => {
+          renderedSubcomponent
+            .find('input')
+            .simulate('change', { currentTarget: { value: testValue } });
+          expect(mockSetOrderOption).toBeCalledTimes(1);
+          expect(mockSetOrderOption).toBeCalledWith({
+            [mockProps.id]: testValue,
+          });
+        });
+
+        break;
+      }
+
+      /* icons */
+      case 'icons': {
+        it('contains input with correct props', () => {
+          const element = renderedSubcomponent.find('div .icon');
+          expect(element).toBeTruthy();
+          expect(element.find('Icon').at(0).prop('name')).toBe(
+            mockProps.values[0].icon
+          );
+          expect(element.find('Icon').at(1).prop('name')).toBe(
+            mockProps.values[1].icon
+          );
+          // Chciałem tutaj sprawdzić też key w tym divie
+        });
+
+        it('should run setOrderOption function on click', () => {
+          renderedSubcomponent.find('div .icon').at(1).simulate('click');
+          expect(mockSetOrderOption).toBeCalledTimes(1);
+          expect(mockSetOrderOption).toBeCalledWith({
+            [mockProps.id]: testValue,
+          });
+        });
+        break;
+      }
+
+      case 'checkboxes': {
+        it('contains input with correct props', () => {
+          const element = renderedSubcomponent.find('.checkboxes');
+
+          expect(element.find('input').at(0).prop('value')).toBe(
+            mockProps.values[0].id
+          );
+          expect(element.find('input').at(0).prop('checked')).toBe(
+            mockProps.currentValue.includes(mockProps.values[0].id)
+          );
+          expect(element.find('input').at(1).prop('value')).toBe(
+            mockProps.values[1].id
+          );
+          expect(element.find('input').at(1).prop('checked')).toBe(
+            mockProps.currentValue.includes(mockProps.values[1].id)
+          );
+        });
+
+        it('should run setOrderOption function on change', () => {
+          renderedSubcomponent
+            .find(`input[value="${testValue}"]`)
+            .simulate('change', { currentTarget: { checked: true } });
+          expect(mockSetOrderOption).toBeCalledTimes(1);
+          expect(mockSetOrderOption).toBeCalledWith({
+            [mockProps.id]: [mockProps.currentValue, testValue],
+          });
+        });
+        break;
+      }
+
+      case 'date': {
+        it('contains component DataPicker', () => {
+          const element = renderedSubcomponent.find('DatePicker');
+          //tutaj też jest coś nie tak. Nizależnie czy wyszuuję DatePicker czy nawet "blablalba" warunek poniżej jest prawdziwy...
+          expect(element).toBeTruthy();
+        });
+
+        /// Nie mam pojęcia jak mam ten DatePicker importować a potem zasymulować event bez renderowania.
+        // it('should run setOrderOption function on change', () => {
+        //   DatePicker.simulate('change', testValue);
+        //   expect(mockSetOrderOption).toBeCalledTimes(1);
+        //   expect(mockSetOrderOption).toBeCalledWith({
+        //     [mockProps.id]: testValue,
+        //   });
+        // });
         break;
       }
     }
